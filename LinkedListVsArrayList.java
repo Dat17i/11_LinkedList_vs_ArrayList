8d84a85d0c8554c3b444a3516a7412adc0b573c6
Claus Bové <clbo@kea.dk>
2018-04-18 22:29:56 +0200

    Create LinkedListVsArrayList.java

---
+++ LinkedListVsArrayList.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LinkedListVsArrayList
+{	
+	
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		List<String> txt = new LinkedList<String>();
+		Scanner scan = new Scanner(new File("/Users/clbo/Google Drive/KEA_Macbook/Eclipse/SWC2/13_Java_Collections_Framework/src/lecture/mobydick.txt"));
+		addToList(scan, txt);
+
+		System.out.println("List is full!");
+		System.out.println("Words in List" + txt.size());
+		
+		while(!txt.isEmpty())
+		{
+			txt.remove(0);
+		}
+		System.out.println("List empty! " + txt);
+	
+	}
+
+	private static void addToList(Scanner scan, List<String> txt)
+	{
+		while(scan.hasNext())
+		{
+			txt.add(scan.next());
+		}
+		
+	}
+	
+}
